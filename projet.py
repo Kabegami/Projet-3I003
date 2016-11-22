45ff1f943dea58215a973b403432bfe014426d13
Kabegami <lucas.becirspahic@gmail.com>
2016-11-22 21:57:27 +0000

    update

--- projet.py
+++ projet.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 
 def lire_sequence(fichier):
+    #Prend un fichier de sequence et retournes les informations dans une liste
     os.chdir('test')
     f = open(fichier,'r')
     sequence = f.read()
@@ -25,6 +26,7 @@ def delta(a, b,dif=1):
 
 
 def cout1(x, y, gap=1,dif=1):
+    #version bottom-up de cout1
     m, n = len(x), len(y)
     F = np.zeros((m+1, n+1), dtype=int)
     for i in range(1, m+1):
@@ -57,12 +59,14 @@ def cout2(x, y, gap=1,dif=1):
 
         
 def cout2bis(x,y,k,l,gap = 1,dif = 1):
+    #on recrer les 2 tableaux a partir de la case i,j
     m,n = len(x)-k, len(y)-l
     F1 = np.zeros(n+1, dtype=int)  # ligne i-1
     F2 = np.zeros(n+1, dtype=int)  # ligne i
     v1 = 1
     for j in range(0,n+1):
         F1[j] = j*gap
+    #Pour avoir 
     for i in range(k,m+k):
         F2[0] = v1*gap
         for j in range(1,n+1):
@@ -77,6 +81,7 @@ def cout2bis(x,y,k,l,gap = 1,dif = 1):
         
 
 def sol1(x, y, F, gap=1,dif=1):
+    """Renvoie l'allignement et les deux chaines avec les gap correspondant"""
     i, j = len(x), len(y)
     M = []
     c1 = ""
@@ -151,6 +156,35 @@ def coutAlign2(c1,c2,gap,dif):
 
     return cout
 
+def SOL2(k1,l1,k2,l2,L,X,Y):
+    if k2 - k1 > 0 or l2 - l1:
+        if k2-k1 <= 2:
+            for i in range(k1,k2+1):
+                X2A[i-k1] = X[i]
+            for j in range(l1,l2+1):
+                Y2A[j-l1] = Y[i]
+            L.append(sol1(X,Y,X2A)[0])
+            L.append(sol1(X,Y,Y2A)[0])
+            return F2A[k2 - k1, l2 - l1]
+        else:
+            if l2 - l1 <= 2:
+                for i in range(k1,k2+1):
+                    X2B[i-k1] = X[i]
+                for j in range(l1,l2+1):
+                    Y2B[j-l1] = Y[i]
+                L.append(sol1(X,Y,X2B)[0])
+                L.append(sol1(X,Y,Y2B)[0])
+                return F2B[k2 - k1, l2 - l1]
+            else:
+                j = l1 + (l2 - l1)//2
+                ip = k1
+                valmin = cout2(k1,j) + cout2bis(k1,j)
+                for i in range(k+1,k2+1):
+                    val = Cout2(i,j) + cout2bis(i,j)
+                    if valmin > val:
+                        valmin = val
+                        ip = i
+                return sol2(k1,l1,ip,j,L) + sol2(ip,j,k2,l2,L)
 
 if __name__ == "__main__":
     L = lire_sequence("Inst_0000010_44.adn")
@@ -161,7 +195,6 @@ if __name__ == "__main__":
     #y = "C"
     t1="AGAGTC"
     t2="T"
-    #l'algorithme marche quand COUT_GAP >= COUT_DIF mais pas l'inverse 
     COUT_GAP = 1
     COUT_DIF = 1
     F = cout1(x,y,COUT_GAP,COUT_DIF)
@@ -169,8 +202,6 @@ if __name__ == "__main__":
     print(F)
     
     M,c1,c2 = sol1(x, y, F,COUT_GAP,COUT_DIF)
-    #print('alignements :', M)
-    #(c1,c2) = affiche(x, y, M)
     print("cout align : ", coutAlign(c1,c2,COUT_GAP,COUT_DIF))
     print("appel de cout2")
     Vcout2 = cout2(x,y,COUT_GAP,COUT_DIF)
@@ -182,3 +213,14 @@ if __name__ == "__main__":
     verif = cout1(t1,t2,COUT_GAP,COUT_DIF)
     print(verif)
     print(verif[len(t1),len(t2)])
+    #----------------------------------------
+    #              Fonction Sol2
+    #----------------------------------------
+    X2A = np.zeros(3)
+    Y2A = np.zeros(len(x)+1)
+    F2A = np.zeros((3,len(x)+1))
+    X2B = np.zeros(len(y)+1)
+    Y2B = np.zeros(3)
+    F2B = np.zeros((3,len(y)+1))
+    L = []
+    test= SOL2(x,len(x),y,len(y),L,x,y)
